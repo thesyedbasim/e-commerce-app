a6a6703e54eb4194d4b07f84ee0ebe136ed1d699
Syed Basim <social.syedbasim@gmail.com>
2022-09-19 23:53:54 +0530

    change pkg name

--- components/product/ProductItem.tsx
+++ components/product/ProductItem.tsx
@@ -1,27 +1,14 @@
 import Link from 'next/link';
 import { ProductMinimal } from '$lib/types/product';
-import { supabase } from '$lib/supabase';
+import { getFirstProductURL } from '$lib/utils/getProductURLSupabase';
 
 const ProductItem: React.FC<{ product: ProductMinimal }> = ({ product }) => {
-	const getProductURL = () => {
-		const { data, error } = supabase.storage
-			.from('images/products')
-			.getPublicUrl(`${product.id}`);
-
-		if (error) {
-			console.error(error);
-			return;
-		}
-
-		return data?.publicURL;
-	};
-
 	return (
 		<div className="card mb-3" style={{ maxWidth: '540px' }}>
 			<div className="row g-0">
 				<div className="col-md-4">
 					<img
-						src={getProductURL()}
+						src={getFirstProductURL(product.id)}
 						className="img-fluid rounded-start"
 						alt={product.name}
 					/>
