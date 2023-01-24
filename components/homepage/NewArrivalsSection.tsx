bdb93d4563e2b5d78421abc01d59b0313813079f
Syed Basim <social.syedbasim@gmail.com>
2023-01-24 22:29:58 +0530

    make home page sections static

--- components/homepage/NewArrivalsSection.tsx
+++ components/homepage/NewArrivalsSection.tsx
@@ -1,34 +1,16 @@
-import { supabase } from '$lib/supabase';
-import { useQuery } from '@tanstack/react-query';
-import Loader from '@components/misc/Loading';
 import SearchProductItem from '@components/search/SearchProductItem';
+import { ProductMinimal } from '$lib/types/product';
 
-const fetchNewArrivalsProducts = async () => {
-	const { data } = await supabase
-		.from('products')
-		.select('id, name, seller, price')
-		.limit(9);
-
-	return data;
-};
-
-const NewArrivalsSection: React.FC = () => {
-	const { data: newArrivalsProducts, isFetching } = useQuery(
-		['newArrivalsProducts'],
-		fetchNewArrivalsProducts
-	);
-
+const NewArrivalsSection: React.FC<{ products: ProductMinimal[] }> = ({
+	products
+}) => {
 	return (
 		<section className="space-y-5">
 			<h2 className="text-3xl font-bold">New arrivals</h2>
 			<div className="grid grid-cols-4 gap-6">
-				{isFetching ? (
-					<Loader />
-				) : (
-					newArrivalsProducts?.map((product) => (
-						<SearchProductItem key={product.id} product={product} />
-					))
-				)}
+				{products?.map((product) => (
+					<SearchProductItem key={product.id} product={product} />
+				))}
 			</div>
 		</section>
 	);
--- components/homepage/categories/Categories.tsx
+++ components/homepage/categories/Categories.tsx
@@ -1,42 +1,17 @@
-import { supabase } from '$lib/supabase';
-import { useQuery } from '@tanstack/react-query';
 import { Category } from '$lib/types/category';
 import CategoryItem from './CategoryItem';
-import Loader from '@components/misc/Loading';
 
-const Categories: React.FC = () => {
-	const { data: categories, isFetching } = useQuery(
-		['categories'],
-		getCategories
-	);
-
+const Categories: React.FC<{ categories: Category[] }> = ({ categories }) => {
 	return (
 		<section className="space-y-5">
 			<h2 className="text-3xl font-bold">Shop by category</h2>
 			<div className="grid grid-cols-4 gap-4">
-				{isFetching ? (
-					<Loader />
-				) : (
-					(categories as Category[])?.map((category) => (
-						<CategoryItem key={category.id} category={category} />
-					))
-				)}
+				{categories.map((category) => (
+					<CategoryItem key={category.id} category={category} />
+				))}
 			</div>
 		</section>
 	);
 };
-
-async function getCategories() {
-	const { data, error } = await supabase
-		.from('categories')
-		.select('id, name, slug, description');
-
-	if (error) {
-		console.error(error);
-		throw new Error('Cannot fetch categories.');
-	}
-
-	return data;
-}
 
 export default Categories;
